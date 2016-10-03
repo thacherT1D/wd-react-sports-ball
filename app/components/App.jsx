83c3353acd6dfe1dc51344d935ebc7334b92f2f7
THACHER <thachert1d@gmail.com>
2016-10-02 21:18:11 -0700

    setup and basics

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState() {
+    return { city: '' };
+  },
+
+  handleChange(event) {
+    const nextState = { city: event.target.value };
+
+    this.setState(nextState);
+  },
+
   render() {
-    return <h1>Hello world</h1>;
+    const city = this.state.city.trim().toLowerCase();
+
+    let message;
+
+    if (city === 'seattle') {
+      message = <ul className="no-bullets">
+        <li className="no-bullets" style={{ margin: '10px' }}>
+          <a href="http://www.soundersfc.com/">Go Sounders!</a>
+        </li>
+        <li style={{ margin: '10px' }}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/2/27/Seattle_Sounders_FC.svg"
+            width="150"
+          />
+        </li>
+      </ul>;
+    }
+    else if (city === 'portland') {
+      message = <ul className="no-bullets">
+        <li style={{ margin: '10px' }}>
+          <a href="http://www.soundersfc.com/">Go Sounders!</a>
+        </li>
+        <li style={{ margin: '10px' }}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/2/27/Seattle_Sounders_FC.svg"
+            width="150"
+          />
+        </li>
+      </ul>;
+    }
+    else if (city === '') {
+      message = <p>Go Ahead, type in a city.</p>;
+    }
+    else {
+      message = <p>{this.state.city}? Never heard of it.</p>;
+    }
+
+    return <div>
+      <h1>
+        {"Favorite City?"}
+      </h1>
+      <input
+        onChange={this.handleChange}
+        type="text"
+        value={this.state.city}
+      />
+      <div>{message}</div>
+    </div>;
   }
+
 });
 
 export default App;
